cf32b4b0a078321d9027f4b6694f09748a3c7d3d
廖敏翔 <35066190+Ridost@users.noreply.github.com>
2025-01-22 12:54:35 +0800

    Update multi-source-bfs.cpp

--- 1765. Map of Highest Peak/multi-source-bfs.cpp
+++ 1765. Map of Highest Peak/multi-source-bfs.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>> highestPeak(vector<vector<int>>& g) {
-        int rows = g.size();
-        int cols = g[0].size();
-        int inf = rows+cols;
-
-        for(int r=0;r<rows;r++) {
-            for(int c=0;c<cols;c++) {
-                if(g[r][c] == 1) g[r][c] = 0;
-                else g[r][c] = 1;
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        queue<pair<int,int>> q;
+        int m = isWater.size();
+        int n = isWater[0].size();
+        vector<vector<int>> height(m,vector<int>(n,INT_MAX));
+        for(int i =0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(isWater[i][j]==1){
+                    q.push(make_pair(i,j));
+                    height[i][j] = 0;
+                }
             }
         }
-
-        for(int r=0;r<rows;r++) {
-            for(int c=0;c<cols;c++) {
-                if(g[r][c] == 0)continue;
-                int left = inf, top = inf;
-                if(c-1 >=0) left = g[r][c-1];
-                if(r-1 >=0) top = g[r-1][c];
-                g[r][c] = min(left,top)+1;
+        int directions[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
+        while(!q.empty()){
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            for(int*direction:directions){
+                int next_x = x+direction[0];
+                int next_y = y+direction[1];
+                if(next_x>=0 && next_x<m && next_y>=0 && next_y<n && height[next_x][next_y]>height[x][y]+1){
+                    q.push(make_pair(next_x,next_y));
+                    height[next_x][next_y] = height[x][y]+1;
+                }
             }
         }
-        for(int r = rows-1; r>=0; r--) {
-            for(int c= cols-1; c>=0; c--) {
-                if(g[r][c] == 0)continue;
-                int right = inf, bottom = inf;
-                if(r+1 < rows) bottom = g[r+1][c];
-                if(c+1 < cols) right = g[r][c+1];
-                g[r][c] = min(g[r][c], min(right,bottom)+1);
-            }
-        }
-        return g;
+        return height;
     }
 };

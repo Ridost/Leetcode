a660029eb95646de2600e2300fb816ec4b4d0c47
Min-Hsiang Liao <35066190+Ridost@users.noreply.github.com>
2025-04-18 10:26:20 +0800

    Update iterative.cpp

--- 38. Count and Say/iterative.cpp
+++ 38. Count and Say/iterative.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        string result = "1";
-
-        for (int i = 2; i <= n; ++i) {
-            string current = "";
-            int count = 1;
-            for (int j = 1; j < result.size(); ++j) {
-                if (result[j] == result[j - 1]) {
-                    count++;
-                } else {
-                    current += to_string(count) + result[j - 1];
-                    count = 1;
-                }
+       string output = "1";
+       for(int i =1;i<n;i++){
+        string next = "";
+        int cnt = 0;
+        char current_c;
+        int len_o = output.size();
+        for(int j = 0;j<len_o;j++){
+            if(j==0) current_c = output[j];
+            if(current_c==output[j]){ 
+                cnt += 1;
             }
-            current += to_string(count) + result.back();
-            result = current;
+            else{
+                next += cnt + '0';
+                next += current_c;
+                cnt = 1;
+                current_c = output[j];
+            }
+            if(j==len_o-1){
+                next += cnt+'0';
+                next += output[j];
+            }
         }
-
-        return result;
+        cout<<output<<endl;
+        output = next;
+       }
+       return output; 
     }
 };
